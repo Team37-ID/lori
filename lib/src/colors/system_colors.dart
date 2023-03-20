3e5a2e81969ed64c12f3fd0d53970f0cccfdca3a
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-03-20 13:53:22 +0700

    refactor: update file name and colors

---
+++ lib/src/colors/system_colors.dart
@@ -0,0 +1,77 @@
+import 'dart:ui';
+
+class SystemColors {
+  /// Primary Colors
+  static const lightPrimary = Color(0xFF4D6700);
+  static const ligthOnPrimary = Color(0xFFFFFFFF);
+  static const lightPrimaryContainer = Color(0xFFC5F356);
+  static const lightOnPrimaryContainer = Color(0xFF151F00);
+
+  static const darkPrimary = Color(0xFFAAD63B);
+  static const darkOnPrimary = Color(0xFF263500);
+  static const darkPrimaryContainer = Color(0xFF394D00);
+  static const darkOnPrimaryContainer = Color(0xFFC5F356);
+
+  /// Secondary Colors
+  static const lightSecondary = Color(0xFF762FE1);
+  static const lightOnSecondary = Color(0xFFFFFFFF);
+  static const lightSecondaryContainer = Color(0xFFEBDCFF);
+  static const lightOnSecondaryContainer = Color(0xFF260058);
+
+  static const darkSecondary = Color(0xFFD4BBFF);
+  static const darkOnSecondary = Color(0xFF40008C);
+  static const darkSecondaryContainer = Color(0xFF5C00C3);
+  static const darkOnSecondaryContainer = Color(0xFFEBDCFF);
+
+  /// Tertiary Colors
+  static const lightTertiary = Color(0xFF4E6700);
+  static const lightOnTertiary = Color(0xFFFFFFFF);
+  static const lightTertiaryContainer = Color(0xFFCEEF7A);
+  static const lightOnTertiaryContainer = Color(0xFF151F00);
+
+  static const darkTertiary = Color(0xFFB2D361);
+  static const darkOnTertiary = Color(0xFF273500);
+  static const darkTertiaryContainer = Color(0xFF3A4D00);
+  static const darkOnTertiaryContainer = Color(0xFFCEEF7A);
+
+  /// Error Colors
+  static const lightError = Color(0xFFBA1A1A);
+  static const lightOnError = Color(0xFFFFFFFF);
+  static const lightErrorContainer = Color(0xFFFFDAD6);
+  static const lightOnErrorContainer = Color(0xFF410002);
+
+  static const darkError = Color(0xFFFFB4AB);
+  static const darkOnError = Color(0xFF690005);
+  static const darkErrorContainer = Color(0xFF93000A);
+  static const darkOnErrorContainer = Color(0xFFFFDAD6);
+
+  /// Utilities Colors
+  static const lightOutline = Color(0xFF76786B);
+  static const lightBackground = Color(0xFFFEFCF4);
+  static const lightOnBackground = Color(0xFF1B1C17);
+  static const lightSurface = Color(0xFFFEFCF4);
+  static const lightOnSurface = Color(0xFF1B1C17);
+  static const lightSurfaceVariant = Color(0xFFE2E4D4);
+  static const lightOnSurfaceVariant = Color(0xFF45483C);
+  static const lightInverseSurface = Color(0xFF30312C);
+  static const lightInverseOnSurface = Color(0xFFF2F1E9);
+  static const lightInversePrimary = Color(0xFFAAD63B);
+  static const lightSurfaceTint = Color(0xFF4D6700);
+  static const lightOutlineVariant = Color(0xFFC6C8B8);
+
+  static const darkOutline = Color(0xFF909284);
+  static const darkBackground = Color(0xFF1B1C17);
+  static const darkOnBackground = Color(0xFFE4E3DB);
+  static const darkSurface = Color(0xFF1B1C17);
+  static const darkOnSurface = Color(0xFFC7C7BF);
+  static const darkSurfaceVariant = Color(0xFF45483C);
+  static const darkOnSurfaceVariant = Color(0xFFC6C8B8);
+  static const darkInverseSurface = Color(0xFFE4E3DB);
+  static const darkInverseOnSurface = Color(0xFF1B1C17);
+  static const darkInversePrimary = Color(0xFF4D6700);
+  static const darkSurfaceTint = Color(0xFFAAD63B);
+  static const darkOutlineVariant = Color(0xFF45483C);
+
+  static const shadow = Color(0xFF000000);
+  static const scrim = Color(0xFF000000);
+}
